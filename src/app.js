d63fa449cae2dc52b325413138a5201502697987
rayenbenhassen07 <rayenbenhassen07@gmail.com>
2024-12-14 22:14:37 +0100

    basic mvc

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Database Connection
+connectDB();
+
+// Routes
+app.get("/", (req, res) => res.send("API is running"));
+app.use("/api/users", userRoutes);
+
+// Export app
+module.exports = app;
